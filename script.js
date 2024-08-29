ef902c02bd890a6813a8c682e47296de83422d87
maksimstep9889MS <maksimstep9889@gmail.com>
2024-08-29 13:02:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,525 @@
+
+/*sliderfirst*/
+window.onload = function () {
+
+let count = 1
+
+setInterval(()=>{
+
+    
+    document.querySelector('.img.show').classList.remove('show')
+    const img = document.querySelector(`.img-${count}`)
+    img.classList.add('show')
+    count++
+
+    if (count > 5){
+        count = 1
+    }
+},1000)
+/*slidersecond*/
+let first = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.amg.show').classList.remove('show')
+    const amg = document.querySelector(`.amg-${first}`)
+    amg.classList.add('show')
+    first++
+
+    if (first > 5) {
+        first = 1
+    }
+}, 1000)
+
+let second = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num3.show').classList.remove('show')
+    const num3 = document.querySelector(`.num3-${second}`)
+    num3.classList.add('show')
+    second++
+
+    if (second > 4) {
+        second = 1
+    }
+}, 1000)
+
+let third = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num4.show').classList.remove('show')
+    const num4 = document.querySelector(`.num4-${third}`)
+    num4.classList.add('show')
+    third++
+
+    if (third > 5) {
+        third = 1
+    }
+}, 1000)
+
+let four = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num5.show').classList.remove('show')
+    const num5 = document.querySelector(`.num5-${four}`)
+    num5.classList.add('show')
+    four++
+
+    if (four > 4) {
+        four = 1
+    }
+}, 1000)
+
+let five = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num6.show').classList.remove('show')
+    const num6 = document.querySelector(`.num6-${five}`)
+    num6.classList.add('show')
+    five++
+
+    if (five > 5) {
+        five = 1
+    }
+}, 1000)
+
+let six = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num7.show').classList.remove('show')
+    const num7 = document.querySelector(`.num7-${six}`)
+    num7.classList.add('show')
+    six++
+
+    if (six > 5) {
+        six = 1
+    }
+}, 1000)
+
+let seven = 1
+
+
+setInterval(() => {
+
+
+    document.querySelector('.num8.show').classList.remove('show')
+    const num8 = document.querySelector(`.num8-${seven}`)
+    num8.classList.add('show')
+    seven++
+
+    if (seven > 5) {
+        seven = 1
+    }
+}, 1000)
+
+
+let eight = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num9.show').classList.remove('show')
+    const num9 = document.querySelector(`.num9-${eight}`)
+    num9.classList.add('show')
+    eight++
+
+    if (eight > 2) {
+        eight = 1
+    }
+}, 1000)
+
+let ten = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num10.show').classList.remove('show')
+    const num10 = document.querySelector(`.num10-${ten}`)
+    num10.classList.add('show')
+    ten++
+
+    if (ten > 2) {
+        ten = 1
+    }
+}, 1000)
+
+let eleven = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num11.show').classList.remove('show')
+    const num11 = document.querySelector(`.num11-${eleven}`)
+    num11.classList.add('show')
+    eleven++
+
+    if (eleven > 2) {
+        eleven = 1
+    }
+}, 1000)
+
+let twelve = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num12.show').classList.remove('show')
+    const num12 = document.querySelector(`.num12-${twelve}`)
+    num12.classList.add('show')
+    twelve++
+
+    if (twelve > 2) {
+        twelve = 1
+    }
+}, 1000)
+
+ let thirteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num13.show').classList.remove('show')
+    const num13 = document.querySelector(`.num13-${thirteen}`)
+    num13.classList.add('show')
+    thirteen++
+
+    if (thirteen > 2) {
+        thirteen = 1
+    }
+}, 1000)
+
+let fourteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num14.show').classList.remove('show')
+    const num14 = document.querySelector(`.num14-${fourteen}`)
+    num14.classList.add('show')
+    fourteen++
+
+    if (fourteen > 2) {
+        fourteen = 1
+    }
+}, 1000)
+
+let fifteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num15.show').classList.remove('show')
+    const num15 = document.querySelector(`.num15-${fifteen}`)
+    num15.classList.add('show')
+    fifteen++
+
+    if (fifteen > 2) {
+        fifteen = 1
+    }
+}, 1000)
+
+let sixteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num16.show').classList.remove('show')
+    const num16 = document.querySelector(`.num16-${sixteen}`)
+    num16.classList.add('show')
+    sixteen++
+
+    if (sixteen > 2) {
+        sixteen = 1
+    }
+}, 1000)
+
+let seventeen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num17.show').classList.remove('show')
+    const num17 = document.querySelector(`.num17-${seventeen}`)
+    num17.classList.add('show')
+    seventeen++
+
+    if (seventeen > 5) {
+        seventeen = 1
+    }
+}, 1000)
+
+let eighteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num18.show').classList.remove('show')
+    const num18 = document.querySelector(`.num18-${eighteen}`)
+    num18.classList.add('show')
+    eighteen++
+
+    if (eighteen > 3) {
+        eighteen = 1
+    }
+}, 1000)
+
+let nineteen = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num19.show').classList.remove('show')
+    const num19 = document.querySelector(`.num19-${nineteen}`)
+    num19.classList.add('show')
+    nineteen++
+
+    if (nineteen > 4) {
+        nineteen = 1
+    }
+}, 1000)
+
+let twenty = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num20.show').classList.remove('show')
+    const num20 = document.querySelector(`.num20-${twenty}`)
+    num20.classList.add('show')
+    twenty++
+
+    if (twenty > 4) {
+        twenty = 1
+    }
+}, 1000)
+
+let twentyone = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num21.show').classList.remove('show')
+    const num21 = document.querySelector(`.num21-${twentyone}`)
+    num21.classList.add('show')
+    twentyone++
+
+    if (twentyone > 2) {
+        twentyone = 1
+    }
+}, 1000)
+
+let twentytwo = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num22.show').classList.remove('show')
+    const num22 = document.querySelector(`.num22-${twentytwo}`)
+    num22.classList.add('show')
+    twentytwo++
+
+    if (twentytwo > 2) {
+        twentytwo = 1
+    }
+}, 1000)
+
+let twentythree = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num23.show').classList.remove('show')
+    const num23 = document.querySelector(`.num23-${twentythree}`)
+    num23.classList.add('show')
+    twentythree++
+
+    if (twentythree > 2) {
+        twentythree = 1
+    }
+}, 1000)
+
+let twentyfour = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.num24.show').classList.remove('show')
+    const num24 = document.querySelector(`.num24-${twentyfour}`)
+    num24.classList.add('show')
+    twentyfour++
+
+    if (twentyfour > 3) {
+        twentyfour = 1
+    }
+}, 1000)
+
+let twentyfive = 1
+
+setInterval(() => {
+
+
+    document.querySelector('.slider-menu1.show').classList.remove('show')
+    const num25 = document.querySelector(`.slider-menu1-${twentyfive}`)
+    num25.classList.add('show')
+    twentyfive++
+
+    if (twentyfive > 6) {
+        twentyfive = 1
+    }
+}, 1000)
+
+    let twentysix = 1
+
+    setInterval(() => {
+
+
+        document.querySelector('.slider_menu2.show').classList.remove('show')
+        const num26 = document.querySelector(`.slider_menu2-${twentysix}`)
+        num26.classList.add('show')
+        twentysix++
+
+        if (twentysix > 4) {
+            twentysix = 1
+        }
+    }, 1000)
+
+
+
+
+
+/* Анимация этапов */
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 10;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal, { passive: true });
+
+function animcard() {
+    var animcards = document.querySelectorAll(".animcard");
+
+    for (var i = 0; i < animcards.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = animcards[i].getBoundingClientRect().top;
+        var elementVisible = 10;
+
+        if (elementTop < windowHeight - elementVisible) {
+            animcards[i].classList.add("active");
+        } else {
+            animcards[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", animcard, { passive: true });
+
+function animcard1() {
+    var animcard1s = document.querySelectorAll(".animcard1");
+
+    for (var i = 0; i < animcard1s.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = animcard1s[i].getBoundingClientRect().top;
+        var elementVisible = 10;
+
+        if (elementTop < windowHeight - elementVisible) {
+            animcard1s[i].classList.add("active");
+        } else {
+            animcard1s[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", animcard1, { passive: true });
+
+function animcard2() {
+    var animcard2s = document.querySelectorAll(".animcard2");
+
+    for (var i = 0; i < animcard2s.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = animcard2s[i].getBoundingClientRect().top;
+        var elementVisible = 10;
+
+        if (elementTop < windowHeight - elementVisible) {
+            animcard2s[i].classList.add("active");
+        } else {
+            animcard2s[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", animcard2, { passive: true });
+
+function animcard3() {
+    var animcard3s = document.querySelectorAll(".animcard3");
+
+    for (var i = 0; i < animcard3s.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = animcard3s[i].getBoundingClientRect().top;
+        var elementVisible = 10;
+
+        if (elementTop < windowHeight - elementVisible) {
+            animcard3s[i].classList.add("active");
+        } else {
+            animcard3s[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", animcard3, { passive: true });
+
+/*Popupform*/
+    document.getElementById("open-modal-btn").addEventListener("click", function () {
+        document.getElementById("my-modal").classList.add("open")
+    })
+
+    // Закрыть модальное окно
+    document.getElementById("close-my-modal-btn").addEventListener("click", function () {
+        document.getElementById("my-modal").classList.remove("open")
+    })
+
+    // Закрыть модальное окно при нажатии на Esc
+    window.addEventListener('keydown', (e) => {
+        if (e.key === "Escape") {
+            document.getElementById("my-modal").classList.remove("open")
+        }
+    });
+
+    // Закрыть модальное окно при клике вне его
+    document.querySelector("#my-modal").addEventListener('click', event => {
+        event._isClickWithInModal = true;
+    });
+    document.getElementById("#my-modal").addEventListener('click', event => {
+        if (event._isClickWithInModal) return;
+        event.currentTarget.classList.remove('open');
+    });
+
+}
